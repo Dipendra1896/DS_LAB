a710dfd379173a75b1cf13c3c0b065f6ae9de3aa
Dipendra1896 <dipendrapatel371896@gmail.com>
2025-06-23 17:15:49 +0545

    Lab-03 completed

---
+++ Lab-03/HelloImpl.java
@@ -0,0 +1,12 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class HelloImpl extends UnicastRemoteObject implements Hello {
+    protected HelloImpl() throws RemoteException {
+        super();
+    }
+
+    public String sayHello() throws RemoteException {
+        return "Hello, this is a remote method call!";
+    }
+}
---
+++ Lab-03/Server.java
@@ -0,0 +1,17 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            HelloImpl obj = new HelloImpl();
+           Registry registry = LocateRegistry.getRegistry();
+            registry.rebind("Hello", obj);
+
+            System.out.println("Server is ready...");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
